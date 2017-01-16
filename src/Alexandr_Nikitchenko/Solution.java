f5819db6b157cd7fb646f669b842d4cee3453268
Alexandr <photoluxe@mail.ru>
2017-01-16 23:15:09 +0600

    Initial commit

---
+++ src/Alexandr_Nikitchenko/Solution.java
@@ -0,0 +1,50 @@
+package Alexandr_Nikitchenko;
+
+/* День недели
+Ввести с клавиатуры номер дня недели, в зависимости от номера вывести название «понедельник», «вторник», «среда», «четверг», «пятница», «суббота», «воскресенье»,
+если введен номер больше или меньше 7 – вывести «такого дня недели не существует».
+Пример для номера 5:
+пятница
+Пример для номера 10:
+такого дня недели не существует
+*/
+
+import java.io.*;
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        int a = Integer.parseInt(s);
+
+        if (a == 1){
+            System.out.println("понедельник");
+        }
+        if (a == 2){
+            System.out.println("вторник");
+        }
+        if (a == 3){
+            System.out.println("среда");
+        }
+        if (a == 4){
+            System.out.println("четверг");
+        }
+        if (a == 5){
+            System.out.println("пятница");
+        }
+        if (a == 6){
+            System.out.println("суббота");
+        }
+        if (a == 7){
+            System.out.println("воскресенье");
+        }
+        if (a > 7 || a <= 0){
+            System.out.println("такого дня недели не существует");
+        }
+
+
+    }
+
+}
